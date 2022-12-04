c26c7a76c7866978837db51adb248fc6535aba3d
Morgentau <mgtauneu@gmail.com>
2022-12-04 09:44:14 +0900

    Add DFT LPF Entrance API

--- src/wave/filter.rs
+++ src/wave/filter.rs
@@ -23,6 +23,13 @@ pub enum EFilter {
         /// クォリティファクタ
         quality_factor: f64,
     },
+    /// DiscreteもしくはFastなFourier Transformを使ってLPFを行う。
+    DFTLowPass {
+        /// エッジ周波数
+        edge_frequency: f64,
+        /// 遷移帯域幅の総周波数範囲
+        delta_frequency: f64,
+    },
 }
 
 struct FIRLowPassInternal {
@@ -33,6 +40,74 @@ struct FIRLowPassInternal {
 }
 
 impl FIRLowPassInternal {
+    fn apply(&self, container: &WaveContainer) -> WaveContainer {
+        // まずLPFでは標本周波数が1として前提して計算を行うので、edgeとdeltaも変換する。
+        let samples_per_sec = container.samples_per_second() as f64;
+        let edge = self.edge_frequency / samples_per_sec;
+        let delta = self.delta_frequency / samples_per_sec;
+
+        // フィルタ係数の数を計算する。
+        // フィルタ係数の数は整数になるしかないし、またJ+1が奇数じゃなきゃならない。
+        // (Window Functionをちゃんと決めるため)
+        let mut j = ((3.1 / delta).round() as isize) - 1;
+        if (j % 2) != 0 {
+            j += 1;
+        }
+
+        // ここではcontainerのチャンネルがMONO(1)だと仮定する。
+        assert!(container.channel() == 1);
+        let filter_bs = {
+            // -J/2からJ/2までにEWindowFunction(Hann)の値リストを求める。
+            let windows = (0..=j)
+                .map(|v| {
+                    let sine = PI2 * ((v as f64) + 0.5) / ((j + 1) as f64);
+                    (1.0 - sine) * 0.5
+                })
+                .collect_vec();
+
+            // フィルタ係数の週はす特性bを計算する。
+            let mut bs = (((j >> 1) * -1)..=(j >> 1))
+                .map(|v| {
+                    let input = PI2 * edge * (v as f64);
+                    let sinc = if input == 0.0 { 1.0 } else { input.sin() / input };
+
+                    2.0 * edge * sinc
+                })
+                .collect_vec();
+
+            assert!(bs.len() == windows.len());
+            for i in 0..windows.len() {
+                bs[i] *= windows[i];
+            }
+
+            bs
+        };
+
+        // bsを用いて折りたたみを行う。
+        let mut new_buffer = vec![];
+        let orig_container = container.uniformed_sample_buffer();
+        new_buffer.resize(orig_container.len(), UniformedSample::default());
+        for i in 0..new_buffer.len() {
+            for ji in 0..=(j as usize) {
+                if i < ji {
+                    break;
+                }
+                new_buffer[i] += filter_bs[ji] * orig_container[i - ji];
+            }
+        }
+
+        WaveContainer::from_uniformed_sample_buffer(container, new_buffer)
+    }
+}
+
+struct DFTLowPassInternal {
+    /// エッジ周波数
+    edge_frequency: f64,
+    /// 遷移帯域幅の総周波数範囲
+    delta_frequency: f64,
+}
+
+impl DFTLowPassInternal {
     fn apply(&self, container: &WaveContainer) -> WaveContainer {
         // まずLPFでは標本周波数が1として前提して計算を行うので、edgeとdeltaも変換する。
         let samples_per_sec = container.samples_per_second() as f64;
@@ -169,6 +244,15 @@ impl EFilter {
             } => IIRLowPassInternal {
                 edge_frequency: *edge_frequency,
                 quality_factor: *quality_factor,
+            }
+            .apply(container),
+            EFilter::DFTLowPass {
+                edge_frequency,
+                delta_frequency,
+            } => DFTLowPassInternal {
+                // ここで書くには長いのでInternal構造体に移して処理を行う。
+                edge_frequency: *edge_frequency,
+                delta_frequency: *delta_frequency,
             }
             .apply(container),
         }
