30f1fb1126539f9bc20a1ea6dc9fb6705411b347
Morgentau <mgtauneu@gmail.com>
2024-08-04 23:27:59 +0900

    倍率対応。2倍までできる

--- tests/ex11/ex11_1.rs
+++ tests/ex11/ex11_1.rs
@@ -3,15 +3,18 @@ use std::{
     io::{self, Write},
 };
 
-use soundprog::wave::{container::WaveContainer, sample::UniformedSample};
+use soundprog::wave::{
+    container::{WaveBuilder, WaveContainer},
+    sample::UniformedSample,
+};
 
 #[test]
 fn ex11_1() {
     //const READ_FILE_PATH: &'static str = "assets/ex11/sine_2s.wav";
     const READ_FILE_PATH: &'static str = "assets/ex6/drum.wav";
     const WRITE_FILE_PATH: &'static str = "assets/ex11/ex11_1_output.wav";
-    const STRETCH_RATE: f64 = 3.0;
-    assert!(STRETCH_RATE > 0.5);
+    const STRETCH_RATE: f64 = 0.5;
+    assert!(STRETCH_RATE >= 0.5);
 
     let wave_container = {
         let source_file = fs::File::open(READ_FILE_PATH).expect(&format!("Could not find {}.", READ_FILE_PATH));
@@ -21,6 +24,7 @@ fn ex11_1() {
     };
 
     // まず音源から周期性を把握する。
+    let shrink_rate = STRETCH_RATE;
     let original_buffer = wave_container.uniformed_sample_buffer();
     let original_samples_size = original_buffer.len();
 
@@ -54,41 +58,102 @@ fn ex11_1() {
             }
         }
 
-        // 元アルゴリズムとは違ってバッファを動的に用意する。
-        // 複雑な波形の場合には固定のバッファだと枠がたりなくなる。
-        out_proceeded_endi = offset_dst + period;
-        if out_proceeded_endi >= out_buffer.len() {
-            let resize_len = out_proceeded_endi.next_power_of_two();
-            out_buffer.resize(resize_len, UniformedSample::default());
-        }
+        debug_assert!(shrink_rate > 0.0, "Given shrink rate is not valid.");
+        match shrink_rate {
+            v if 0.0 < v && v < 1.0 => {
+                // 元アルゴリズムとは違ってバッファを動的に用意する。
+                // 複雑な波形の場合には固定のバッファだと枠がたりなくなる。
+                out_proceeded_endi = offset_dst + (period * 2);
+                if out_proceeded_endi >= out_buffer.len() {
+                    let resize_len = (out_proceeded_endi + 1).next_power_of_two();
+                    out_buffer.resize(resize_len, UniformedSample::default());
+                }
 
-        for n in 0..period {
-            // 単調減少の重み付け。Lerpっぽくする。
-            let b_factor = (n as f64) / (period as f64);
-            let a_factor = 1.0 - b_factor;
-            out_buffer[offset_dst + n] = a_factor * original_buffer[offset_src + n];
-            out_buffer[offset_dst + n] += b_factor * original_buffer[offset_src + period + n];
-        }
+                // 伸ばす
+                for n in 0..period {
+                    out_buffer[offset_dst + n] = original_buffer[offset_src + n];
 
-        let q_param = ((period as f64) / (STRETCH_RATE - 1.0)).round() as usize;
-        for n in period..q_param {
-            if offset_src + period + n >= original_samples_size {
-                break;
+                    // 単調減少の重み付け。Lerpっぽくする。
+                    let b_factor = (n as f64) / (period as f64);
+                    let a_factor = 1.0 - b_factor;
+
+                    let out_i = offset_dst + period + n;
+                    out_buffer[out_i] = a_factor * original_buffer[offset_src + period + n];
+                    out_buffer[out_i] += b_factor * original_buffer[offset_src + n];
+                }
+
+                let q_param = ((period as f64 * shrink_rate) / (1.0 - shrink_rate)).round() as usize;
+                if q_param > period {
+                    for n in period..q_param {
+                        if offset_src + n >= original_samples_size {
+                            break;
+                        }
+
+                        let dst_len = offset_dst + period + n;
+                        out_proceeded_endi = dst_len;
+                        if out_proceeded_endi >= out_buffer.len() {
+                            let resize_len = (out_proceeded_endi + 1).next_power_of_two();
+                            out_buffer.resize(resize_len, UniformedSample::default());
+                        }
+
+                        out_buffer[dst_len] = original_buffer[offset_src + n];
+                    }
+                }
+
+                offset_src += q_param;
+                offset_dst += period + q_param;
             }
+            v if v > 1.0 => {
+                // 元アルゴリズムとは違ってバッファを動的に用意する。
+                // 複雑な波形の場合には固定のバッファだと枠がたりなくなる。
+                out_proceeded_endi = offset_dst + period;
+                if out_proceeded_endi >= out_buffer.len() {
+                    let resize_len = (out_proceeded_endi + 1).next_power_of_two();
+                    out_buffer.resize(resize_len, UniformedSample::default());
+                }
 
-            out_buffer[offset_dst + n] = original_buffer[offset_src + period + n];
+                // 縮ませる
+                for n in 0..period {
+                    // 単調減少の重み付け。Lerpっぽくする。
+                    let b_factor = (n as f64) / (period as f64);
+                    let a_factor = 1.0 - b_factor;
+
+                    let in_i = offset_dst + n;
+                    out_buffer[in_i] = a_factor * original_buffer[offset_src + n];
+                    out_buffer[in_i] += b_factor * original_buffer[offset_src + period + n];
+                }
+
+                let q_param = ((period as f64) / (shrink_rate - 1.0)).round() as usize;
+                if q_param > period {
+                    for n in period..q_param {
+                        if offset_src + period + n >= original_samples_size {
+                            break;
+                        }
+
+                        out_buffer[offset_dst + n] = original_buffer[offset_src + period + n];
+                    }
+                }
+
+                offset_src += period + q_param;
+                offset_dst += q_param;
+            }
+            v if v == 1.0 => {}
+            _ => {}
         }
-
-        offset_src += period + q_param;
-        offset_dst += q_param;
     }
     let _ = out_buffer.split_off(out_proceeded_endi);
 
-    let write_container = WaveContainer::from_uniformed_sample_buffer(&wave_container, out_buffer);
+    let new_wave_container = WaveBuilder {
+        samples_per_sec: wave_container.samples_per_second(),
+        bits_per_sample: wave_container.bits_per_sample() as u16,
+    }
+    .build_container(out_buffer)
+    .unwrap();
+
     {
         let dest_file = fs::File::create(WRITE_FILE_PATH).expect(&format!("Could not create {}.", WRITE_FILE_PATH));
         let mut writer = io::BufWriter::new(dest_file);
-        write_container.write(&mut writer);
+        new_wave_container.write(&mut writer);
         writer.flush().expect("Failed to flush writer.")
     }
 }
