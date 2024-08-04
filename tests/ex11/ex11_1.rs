bb8496626850af3335871a94f7141041db3c3c42
Morgentau <mgtauneu@gmail.com>
2024-08-04 21:58:12 +0900

    リファクタリングとバッファの動的生成でバフ修正

--- tests/ex11/ex11_1.rs
+++ tests/ex11/ex11_1.rs
@@ -10,7 +10,7 @@ fn ex11_1() {
     //const READ_FILE_PATH: &'static str = "assets/ex11/sine_2s.wav";
     const READ_FILE_PATH: &'static str = "assets/ex6/drum.wav";
     const WRITE_FILE_PATH: &'static str = "assets/ex11/ex11_1_output.wav";
-    const STRETCH_RATE: f64 = 10.0;
+    const STRETCH_RATE: f64 = 3.0;
     assert!(STRETCH_RATE > 0.5);
 
     let wave_container = {
@@ -25,79 +25,64 @@ fn ex11_1() {
     let original_samples_size = original_buffer.len();
 
     let original_fs = wave_container.samples_per_second();
-    let processed_samples_size = {
-        let samples_size = original_samples_size as f64 / STRETCH_RATE as f64;
-        samples_size.ceil() as usize
-    };
     let template_size = (original_fs as f64 * 0.01) as usize;
     let p_min = (original_fs as f64 * 0.005) as usize;
     let p_max = (original_fs as f64 * 0.02) as usize;
 
-    let mut x_buffer = vec![];
-    x_buffer.resize(template_size, UniformedSample::default());
-    let mut y_buffer = vec![];
-    y_buffer.resize(template_size, UniformedSample::default());
-    let mut r_buffer = vec![];
-    r_buffer.resize(p_max + 1, UniformedSample::default());
-
+    let mut out_proceeded_endi = 0;
     let mut out_buffer = vec![];
-    out_buffer.resize(processed_samples_size, UniformedSample::default());
+    out_buffer.resize(1, UniformedSample::default());
 
-    let mut offset0 = 0;
-    let mut offset1 = 0;
-    while (offset0 + (p_max * 2)) < original_samples_size {
-        {
-            let start_i = offset0;
-            let end_i = start_i + template_size;
-            x_buffer.copy_from_slice(&original_buffer[start_i..end_i]);
-        }
-
-        // mサンプルずらして記入
+    let mut offset_src = 0;
+    let mut offset_dst = 0;
+    while (offset_src + (p_max * 2)) < original_samples_size {
+        // mサンプルずらして記入。一種のConvolutionを行う。
+        // 一番Peak(正の数)が波形の周期だとみなす。
         let mut r_max = 0.0;
         let mut period = p_min;
         for m in p_min..=p_max {
-            {
-                let start_i = offset0 + m;
-                let end_i = start_i + template_size;
-                y_buffer.copy_from_slice(&original_buffer[start_i..end_i]);
-            }
-
-            let mut result = UniformedSample::default();
+            let mut result = 0.0;
             for n in 0..template_size {
-                //let y_index = offset0 + m + n;
-                result += x_buffer[n] * y_buffer[n];
+                let x_index = offset_src + n;
+                let y_index = offset_src + m + n;
+                result += original_buffer[x_index].to_f64() * original_buffer[y_index].to_f64();
             }
 
-            if result.to_f64() > r_max {
-                r_max = result.to_f64();
+            if result > r_max {
+                r_max = result;
                 period = m;
             }
+        }
 
-            r_buffer[m] = result;
+        // 元アルゴリズムとは違ってバッファを動的に用意する。
+        // 複雑な波形の場合には固定のバッファだと枠がたりなくなる。
+        out_proceeded_endi = offset_dst + period;
+        if out_proceeded_endi >= out_buffer.len() {
+            let resize_len = out_proceeded_endi.next_power_of_two();
+            out_buffer.resize(resize_len, UniformedSample::default());
         }
 
         for n in 0..period {
             // 単調減少の重み付け。Lerpっぽくする。
             let b_factor = (n as f64) / (period as f64);
             let a_factor = 1.0 - b_factor;
-            out_buffer[offset1 + n] = a_factor * original_buffer[offset0 + n];
-            out_buffer[offset1 + n] += b_factor * original_buffer[offset0 + period + n];
+            out_buffer[offset_dst + n] = a_factor * original_buffer[offset_src + n];
+            out_buffer[offset_dst + n] += b_factor * original_buffer[offset_src + period + n];
         }
 
         let q_param = ((period as f64) / (STRETCH_RATE - 1.0)).round() as usize;
         for n in period..q_param {
-            if offset0 + period + n >= original_samples_size {
+            if offset_src + period + n >= original_samples_size {
                 break;
             }
 
-            out_buffer[offset1 + n] = original_buffer[offset0 + period + n];
+            out_buffer[offset_dst + n] = original_buffer[offset_src + period + n];
         }
 
-        offset0 += period + q_param;
-        offset1 += q_param;
-
-        println!("{}, {}, {}, {}", offset0, offset1, period, q_param);
+        offset_src += period + q_param;
+        offset_dst += q_param;
     }
+    let _ = out_buffer.split_off(out_proceeded_endi);
 
     let write_container = WaveContainer::from_uniformed_sample_buffer(&wave_container, out_buffer);
     {
